f58cc36e5bf40a0900e26fdf4ee3e9e56247e8af
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-12-16 13:57:15 +0000

    add error src without any styles

--- src/components/ErrorMessage/Error.jsx
+++ src/components/ErrorMessage/Error.jsx
@@ -2,7 +2,7 @@ import ErrorImg from './error_img.jpg';
 
 const ErrorMessage = () => {
     return (
-        <img alt="error_image" style={{ width: 200, height: 200, margin: 'auto' }} />
+        <img src={ErrorImg} alt="error_image" style={{ width: 200, height: 200, margin: 'auto' }} />
     )
 }
 
